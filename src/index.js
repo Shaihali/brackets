f4dfb5393e57af7695351380e66ae9a8bf28f468
Shaihali <mr.saybulaev@mail.ru>
2023-02-05 23:11:43 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = ''
+  let arr = []
+
+  bracketsConfig.forEach(element => {
+    config += element.join('')
+  })
+  for (let i = 0; i < str.length; i++) {
+          let index = config.indexOf(str[i])
+          if (index % 2 === 0) {
+              if (str[i] === config[index + 1] && arr[arr.length - 1] === index){
+                arr.pop();
+              } else if (str[i] === config[index + 1] && arr[arr.length - 1] !== index) {
+                arr.push(index)
+              } else{
+                arr.push(index)
+              }
+          } else {
+              if (arr.pop() !== index -1){
+                  return false;
+              }
+          }
+      }
+      return arr.length === 0
 }
